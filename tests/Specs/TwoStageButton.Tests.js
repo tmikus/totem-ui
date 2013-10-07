f750b1464a027c21e320a67dd0deff6a1280ea8d
Tomasz Mikuś <mikus.tomasz@gmail.com>
2013-10-07 12:27:28 +0200

    TUI-30: Write unit tests for the TwoStageButton control.

--- tests/Specs/TwoStageButton.Tests.js
+++ tests/Specs/TwoStageButton.Tests.js
@@ -23,5 +23,42 @@ describe("TwoStageButton", function () {
 
     it("should have correct content after initialization", function () {
         expect($buttonElement.children(":visible").text()).toEqual("");
-    })
+    });
+
+    it ("shouldn't throw exception after first click on the button", function () {
+        function clickButton() {
+            $buttonElement.click();
+        }
+        expect(clickButton).not.toThrow();
+    });
+
+    it("should have correct content after clicking", function () {
+        $buttonElement.click();
+        expect($buttonElement.children(":visible").text()).toEqual(TotemUI.TwoStageButton.defaultConfiguration.confirmMessage);
+    });
+
+    it("should restore content to original state after configured timeout", function () {
+        jasmine.Clock.useMock();
+
+        // Checking content if it is normal
+        expect($buttonElement.children(":visible").text()).toEqual("");
+
+        // Clicking on the button to show the confirmation message
+        $buttonElement.click();
+
+        // Checking if content has confirm message
+        expect($buttonElement.children(":visible").text()).toEqual(TotemUI.TwoStageButton.defaultConfiguration.confirmMessage);
+
+        // Calling mouse leave event
+        $buttonElement.mouseleave();
+
+        // Making sure content is ok
+        expect($buttonElement.children(":visible").text()).toEqual(TotemUI.TwoStageButton.defaultConfiguration.confirmMessage);
+
+        // Calling timeout
+        jasmine.Clock.tick(TotemUI.TwoStageButton.defaultConfiguration.clickTimeout + 1);
+
+        // Checking if content went back to normal
+        expect($buttonElement.children(":visible").text()).toEqual("");
+    });
 });
